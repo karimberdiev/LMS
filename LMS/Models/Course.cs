7a8d8dd6b4aa05ce5fbe79a45fdba90d03643d9c
karimberdiev <azizbekkmail@gmail.com>
2025-11-26 19:35:32 +0500

    nimadir

--- LMS/Models/Course.cs
+++ LMS/Models/Course.cs
@@ -8,5 +8,6 @@ namespace LMS.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public int credit { get; set; }
     }
 }
